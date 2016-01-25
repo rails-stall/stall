9d425b6f7122c3cbe1726936cd2201791e337521
vala <gonjo@free.fr>
2016-01-25 17:41:57 +0100

    do not test generators for now, and add base calculator tests

---
+++ spec/services/shipping_calculators/calculator_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Stall::Shipping::Calculator do
+  describe '.register' do
+    it 'allows registering a calculator to the global calculators hash' do
+      Stall::Shipping::Calculator.register('fake')
+      expect(Stall::Shipping.calculators['fake']).to eq(Stall::Shipping::Calculator)
+    end
+  end
+
+  describe '#available_for?' do
+    it 'raises when not overriden' do
+      calculator = Stall::Shipping::Calculator.new(double(:cart), double(:config))
+      expect { calculator.available_for?(double(:address)) }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe '#price' do
+    it 'raises when not overriden' do
+      calculator = Stall::Shipping::Calculator.new(double(:cart), double(:config))
+      expect { calculator.price }.to raise_error(NoMethodError)
+    end
+  end
+end
