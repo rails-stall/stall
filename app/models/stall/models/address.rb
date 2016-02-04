b1a5a1929a80155e404918d48207ed64d7853bd0
vala <gonjo@free.fr>
2016-02-04 14:45:42 +0100

    allow enumerating and getting translated civilities

--- app/models/stall/models/address.rb
+++ app/models/stall/models/address.rb
@@ -7,6 +7,18 @@ module Stall
         self.table_name = 'stall_addresses'
 
         has_one :addressable_ownership, dependent: :destroy
+
+        enum civility: { :m => 1, :mme => 2 }
+
+        def self.civility_enum
+          civilities.each_with_object({}) do |(key, value), hash|
+            hash[I18n.t("stall.addresses.civilities.#{ key }")] = key
+          end
+        end
+
+        def civility_name
+          I18n.t("stall.addresses.civilities.#{ civility }")
+        end
       end
     end
   end
