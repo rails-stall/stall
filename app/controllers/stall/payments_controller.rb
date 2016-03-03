6fa007d95a784e240cbeb45ed6a156c087aa466a
vala <gonjo@free.fr>
2016-03-03 19:02:24 +0100

    do not protect from CSRF in payments controller since all clients are authenticated by the gateway mechanism

--- app/controllers/stall/payments_controller.rb
+++ app/controllers/stall/payments_controller.rb
@@ -1,5 +1,7 @@
 module Stall
   class PaymentsController < Stall::ApplicationController
+    skip_before_action :verify_authenticity_token, only: [:notify]
+
     def notify
       service = Stall::PaymentNotificationService.new(params[:gateway], request)
       service.call
