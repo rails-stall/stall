5a554ad22a0d9723396e48340133675c56955ee4
vala <gonjo@free.fr>
2016-09-26 17:18:35 +0200

    handle payment return to PaymentCheckoutStep after payment

--- lib/stall/checkout/payment_checkout_step.rb
+++ lib/stall/checkout/payment_checkout_step.rb
@@ -4,6 +4,13 @@ module Stall
       def process
         return true if params[:succeeded]
       end
+
+      # When we access this step after a payment to validate the step, the cart
+      # is "inactive", so we force processing the cart.
+      #
+      def allow_inactive_carts?
+        !!params[:succeeded]
+      end
     end
   end
 end
