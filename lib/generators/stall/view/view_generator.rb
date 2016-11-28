f83dbe035ac08d9a2d4f858ae060f6537b91cd3e
vala <gonjo@free.fr>
2016-11-28 18:00:56 +0100

    auto-detect partials in stall:view generator

--- lib/generators/stall/view/view_generator.rb
+++ lib/generators/stall/view/view_generator.rb
@@ -1,37 +1,59 @@
 module Stall
   class ViewGenerator < ::Rails::Generators::Base
-    class ViewNotFound < StandardError; end
-
     VIEWS_DIR = File.expand_path('../../../../../app/views', __FILE__)
     source_root VIEWS_DIR
 
     argument :file_paths, type: :array, required: true
 
     def copy_view_template
-      paths = file_paths.map do |file_path|
-        source_path = source_path_for(file_path)
-        raise ViewNotFound, "Could not find any stall view for #{ file_path }" unless File.exist?(source_path)
-        target_path = "app/views/#{ file_path_with_ext_for(file_path) }"
+      paths = file_paths.map { |file_path| ViewTemplate.new(file_path) }
 
-        [source_path, target_path]
-      end
-
-      paths.each do |source_path, target_path|
-        template(source_path, target_path)
+      paths.each do |view_template|
+        template(view_template.source_path, view_template.target_path)
       end
     end
 
-    private
 
-    def source_path_for(file_path)
-      File.join(VIEWS_DIR, file_path_with_ext_for(file_path))
-    end
+    class ViewTemplate
+      class ViewNotFound < StandardError; end
 
-    def file_path_with_ext_for(file_path)
-      if file_path.match(/\.html\.haml\z/)
-        file_path
-      else
-        "#{ file_path }.html.haml"
+      attr_reader :file_path
+
+      def initialize(file_path)
+        @file_path = file_path
+      end
+
+      def source_path
+        source_file_for(file_path_with_ext)
+      end
+
+      def target_path
+        "app/views/#{ file_path_with_ext }"
+      end
+
+      private
+
+      def file_path_with_ext
+        return @file_path_with_ext if @file_path_with_ext
+        @file_path += '.html.haml' unless file_path.match(/\.html\.haml\z/)
+
+        if File.exist?(source_file_for(file_path))
+          @file_path_with_ext = file_path
+          return @file_path_with_ext
+        end
+
+        partial_path = [File.dirname(file_path), File.basename(file_path)].join('/_')
+
+        if File.exist?(source_file_for(partial_path))
+          @file_path_with_ext = partial_path
+          return @file_path_with_ext
+        end
+
+        raise ViewNotFound, "No Stall view was found for #{ file_path } !"
+      end
+
+      def source_file_for(path)
+        File.join(Stall::ViewGenerator::VIEWS_DIR, path)
       end
     end
   end
