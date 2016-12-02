4c6d36e3597da52956dbce025171886b32e95193
vala <gonjo@free.fr>
2016-12-02 09:37:06 +0100

    more CustomerHelper specs

--- spec/helpers/customers_helper_spec.rb
+++ spec/helpers/customers_helper_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 
 RSpec.describe Stall::CustomersHelper do
+  describe '#stall_user_signed_in?' do
+    it 'returns true when there is a signed in user' do
+      allow(helper).to receive(:current_stall_user).and_return(build(:user))
+      expect(helper.stall_user_signed_in?).to eq(true)
+    end
+
+    it 'returns false when there is no signed in user' do
+      allow(helper).to receive(:current_stall_user).and_return(nil)
+      expect(helper.stall_user_signed_in?).to eq(false)
+    end
+  end
+
+  describe '#current_stall_user' do
+    it 'returns the signed in user' do
+      user = build(:user)
+
+      allow(Stall.config).to receive(:default_user_helper_method).and_return(:current_user)
+      allow(helper).to receive(:current_user).and_return(user)
+
+      expect(helper.current_stall_user).to eq(user)
+    end
+
+    it 'returns nil when there is no signed in user' do
+      allow(Stall.config).to receive(:default_user_helper_method).and_return(:current_user)
+      allow(helper).to receive(:current_user).and_return(nil)
+
+      expect(helper.current_stall_user).to eq(nil)
+    end
+
+    it 'returns nil when there is no configured user model' do
+      allow(Stall.config).to receive(:default_user_helper_method).and_return(nil)
+      expect(helper.current_stall_user).to eq(nil)
+    end
+  end
+
   describe '#with_errors_from_user' do
     it 'adds the user e-mail errors to the customer errors' do
       user = build(:user)
