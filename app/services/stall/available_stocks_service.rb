a5e2038bd1aeef5b4f5e6528092265c3cdad745a
vala <gonjo@free.fr>
2017-04-21 05:17:22 +0200

    fix stock availability issue

--- app/services/stall/available_stocks_service.rb
+++ app/services/stall/available_stocks_service.rb
@@ -2,7 +2,7 @@ module Stall
   class AvailableStocksService < Stall::BaseService
     def on_hand?(line_item)
       if Stall.config.manage_inventory
-        line_item.variant.stock >= line_item.quantity
+        line_item.sellable.stock >= line_item.quantity
       else
         true
       end
