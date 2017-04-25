372a12a46ce75d77ba530d6d8dc1620cc43bf73b
vala <gonjo@free.fr>
2017-04-25 14:14:20 +0200

    split shipping fee calculator service #call into smaller methods

--- app/services/stall/shipping_fee_calculator_service.rb
+++ app/services/stall/shipping_fee_calculator_service.rb
@@ -9,24 +9,33 @@ module Stall
     def call
       return unless cart.shipment && cart.shipment.shipping_method
 
-      calculator_identifier = cart.shipment.shipping_method.identifier
-      calculator_class = Stall::Shipping::Calculator.for(calculator_identifier)
-
-      if calculator_class
-        calculator = calculator_class.new(cart, cart.shipment.shipping_method)
-
-        cart.shipment.update_attributes(
-          price: calculator.price,
-          eot_price: calculator.eot_price,
-          vat_rate: calculator.vat_rate
-        )
-      end
+      update_price_for(cart.shipment, calculator) if calculator
     end
 
     def available?
       cart.line_items.length > 0 &&
-      cart.try(:shipping_address) &&
-      cart.try(:shipment).try(:shipping_method)
+        cart.try(:shipping_address) &&
+          cart.try(:shipment).try(:shipping_method)
+    end
+
+    private
+
+    def update_price_for(shipment, calculator)
+      shipment.update_attributes(
+        price: calculator.price,
+        eot_price: calculator.eot_price,
+        vat_rate: calculator.vat_rate
+      )
+    end
+
+    def calculator
+      @calculator ||= calculator_class && calculator_class.new(cart, cart.shipment.shipping_method)
+    end
+
+    def calculator_class
+      @calculator_class ||= Stall::Shipping::Calculator.for(
+        cart.shipment.shipping_method.identifier
+      ) if cart.shipment.try(:shipping_method)
     end
   end
 end
