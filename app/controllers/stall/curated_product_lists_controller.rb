c175f7977b2bbc63f923b7b2c788c0d43903c8b8
vala <gonjo@free.fr>
2017-05-01 11:44:32 +0200

    fix curated product lists index issue when ordered

--- app/controllers/stall/curated_product_lists_controller.rb
+++ app/controllers/stall/curated_product_lists_controller.rb
@@ -5,6 +5,12 @@ module Stall
     def show
       @curated_product_list = CuratedProductList.friendly.find(params[:id])
       search_products_among(@curated_product_list.products)
+
+      # Also select curated list product positions to allow distinct call to
+      # work when ordering results by position
+      @products = @products.select('stall_products.*, stall_curated_list_products.position')
+
+      add_breadcrumb(@curated_product_list)
     end
   end
 end
