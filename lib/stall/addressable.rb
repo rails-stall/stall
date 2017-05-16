65aef612598ee6f92e2f24aac2bc0cdd8462ee6a
vala <gonjo@free.fr>
2017-05-16 16:07:03 +0200

    fix nested attributes assignation

--- lib/stall/addressable.rb
+++ lib/stall/addressable.rb
@@ -23,22 +23,53 @@ module Stall
 
     # Allow billing address to fall back to shipping address when not filled
     def billing_address
-      association(:billing_address).load_target ||
+      if (billing_address = association(:billing_address).load_target)
+        billing_address
+      elsif !@_force_actual_address_association
         association(:shipping_address).load_target
+      end
     end
 
     def billing_address?
       billing_address.try(:persisted?) && billing_address.billing?
     end
 
+    def billing_address_attributes=(attributes)
+      with_actual_address_associations do
+        assign_nested_attributes_for_one_to_one_association(:billing_address, attributes)
+      end
+    end
+
     # Allow shipping address to fall back to billing address when not filled
     def shipping_address
-      association(:shipping_address).load_target ||
+      if (shipping_address = association(:shipping_address).load_target)
+        shipping_address
+      elsif !@_force_actual_address_association
         association(:billing_address).load_target
+      end
     end
 
     def shipping_address?
-      shipping_address.try(:persisted?) && shipping_address.shipping?
+      shipping_address.try(:persisted?) && shipping_address.billing?
+    end
+
+    def shipping_address_attributes=(attributes)
+      with_actual_address_associations do
+        assign_nested_attributes_for_one_to_one_association(:shipping_address, attributes)
+      end
+    end
+
+    # Allow forcing actual address associations to be retrieved, thus disabling
+    # addresses fallback for the duration of the block.
+    #
+    # This is used for nested attributes assignation which calls the
+    # associations reader directly, which happen to merge addresses when
+    # both billing and shipping addresses are assigned at the same time.
+    #
+    def with_actual_address_associations
+      @_force_actual_address_association = true
+      yield
+      @_force_actual_address_association = false
     end
   end
 end
