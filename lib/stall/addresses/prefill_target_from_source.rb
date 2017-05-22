01f9af69bf82f8be0e2a4b72459f73f2b68b31b9
vala <gonjo@free.fr>
2017-05-22 09:13:46 +0200

    fix addresses management in checkout

--- lib/stall/addresses/prefill_target_from_source.rb
+++ lib/stall/addresses/prefill_target_from_source.rb
@@ -9,13 +9,17 @@ module Stall
       private
 
       def prefill_address(type)
-        source_address = source.send("#{ type }_address")
+        source.with_actual_address_associations do
+          target.with_actual_address_associations do
+            source_address = source.send("#{ type }_address")
 
-        if source_address && !target.send("#{ type }_address")
-          attributes = duplicate_attributes(source_address)
-          target.send("build_#{ type }_address", attributes)
-        elsif !target.send("#{ type }_address")
-          target.send("build_#{ type }_address")
+            if source_address && !target.send("#{ type }_address?")
+              attributes = duplicate_attributes(source_address)
+              target.send("build_#{ type }_address", attributes)
+            elsif !target.send("#{ type }_address?")
+              target.send("build_#{ type }_address")
+            end
+          end
         end
       end
     end
